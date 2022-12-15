0d2c851c83487fcc9de84dd15eea40c69c24a62d
mike.girenko <mike.girenko@cybergrx.com>
2022-12-15 15:11:21 -0700

    Completed Life in Weeks

--- life_in_weeks/life_in_weeks.py
+++ life_in_weeks/life_in_weeks.py
@@ -2,20 +2,30 @@
 Create a program that tells how many days, weeks, and months we have until 90 years old
 """
 
-
-def life_in_weeks():
-    your_current_age = input("Enter your current age ")
-    days_left = (90 - int(your_current_age)) * 365
-    weeks_left = (90 - int(your_current_age)) * 56
-    months_left = (90 - int(your_current_age)) * 12
-    print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
-
-
-def calculate_days_left():
-    your_current_age = 50
-    days_left = (90 - your_current_age) * 365
+def calculate_days_left(current_age) -> int:
+    days_left = (90 - current_age) * 365
 
     return days_left
+
+
+def calculate_weeks_left(current_age) -> int:
+    weeks_left = (90 - current_age) * 56
+
+    return weeks_left
+
+
+def calculate_months_left(current_age) -> int:
+    months_left = (90 - current_age) * 12
+
+    return months_left
+
+
+def life_in_weeks():
+    your_current_age = int(input("Enter your current age "))
+    days_left = calculate_days_left(your_current_age)
+    weeks_left = calculate_weeks_left(your_current_age)
+    months_left = calculate_months_left(your_current_age)
+    print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
 
 
 if __name__ == "__main__":
--- life_in_weeks/test_life_in_weeks.py
+++ life_in_weeks/test_life_in_weeks.py
@@ -1,7 +1,14 @@
-import pytest
+from life_in_weeks.life_in_weeks import calculate_days_left, calculate_weeks_left, \
+    calculate_months_left
 
-from life_in_weeks.life_in_weeks import calculate_days_left
+
+def test_calculate_days_left():
+    assert calculate_days_left(50) == 14600
 
 
-def test_calculate():
-    assert calculate_days_left() == 14600
+def test_calculate_weeks_left():
+    assert calculate_weeks_left(50) == 2240
+
+
+def test_calculate_months_left():
+    assert calculate_months_left(50) == 480
