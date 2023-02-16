5db3fae37d36409501f58eab98030f0f201f12d5
mike.girenko <mike.girenko@cybergrx.com>
2023-02-16 15:08:34 -0700

    Refactored to use elif

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -13,13 +13,13 @@ class Bmi:
     def interpret_bmi(self, bmi):
         if bmi < 18.5:
             print(f"Your BMI is {bmi}, you are underweight.")
-        if 18.5 < bmi < 25:
+        elif bmi < 25:
             print(f"Your BMI is {bmi}, you have a normal weight.")
-        if 25 < bmi < 30:
+        elif bmi < 30:
             print(f"Your BMI is {bmi}, you are slightly overweight.")
-        if 30 < bmi < 35:
+        elif bmi < 35:
             print(f"Your BMI is {bmi}, you are obese.")
-        if 35 < bmi:
+        else:
             print(f"Your BMI is {bmi}, you are clinically obese.")
 
 
