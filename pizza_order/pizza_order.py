77f2062113254bc10decce489277a0ae2d5bc44b
mike.girenko <mike.girenko@cybergrx.com>
2023-03-07 15:34:50 -0700

    Completed Pizza Order homework

--- pizza_order/pizza_order.py
+++ pizza_order/pizza_order.py
@@ -1,24 +1,29 @@
-
 class PizzaOrder:
     def user_input(self) -> tuple:
         size = input("Enter pizza size - S, M, or L ")
         pepperoni = input("Add pepperoni - Y or N ")
-        cheese = input("Add extra cheese - Y or N ")
+        extra_cheese = input("Add extra cheese - Y or N ")
 
-        return size, pepperoni, cheese
+        return size, pepperoni, extra_cheese
 
-
-    def calculate_bill(self, size, pepperoni, cheese) -> int:
+    def calculate_bill(self, size, pepperoni, extra_cheese) -> int:
         bill = 0
         if size == "S":
             bill = 15
+            if pepperoni == "Y":
+                bill = bill + 2
         if size == "M":
             bill = 20
+            if pepperoni == "Y":
+                bill = bill + 3
         if size == "L":
             bill = 25
+            if pepperoni == "Y":
+                bill = bill + 3
+        if extra_cheese == "Y":
+            bill = bill + 1
 
         return bill
-
 
     def print_output(self, bill):
         print(f"Your final bill is: ${bill}.")
@@ -26,5 +31,6 @@ class PizzaOrder:
 
 if __name__ == "__main__":
     obj = PizzaOrder()
-    print("printing", obj.user_input())
-    obj.print_output(28)
+    size, pepperoni, extra_cheese = obj.user_input()
+    bill = obj.calculate_bill(size, pepperoni, extra_cheese)
+    obj.print_output(bill)
--- pizza_order/test_pizza_order.py
+++ pizza_order/test_pizza_order.py
@@ -4,6 +4,37 @@ obj = PizzaOrder()
 
 
 def test_calculate_bill():
-    assert obj.calculate_bill("S", "Y", "N") == 15
+    size = "S"
+    pepperoni = "N"
+    extra_cheese = "N"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 15
 
+    size = "S"
+    pepperoni = "Y"
+    extra_cheese = "Y"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 18
 
+    size = "M"
+    pepperoni = "N"
+    extra_cheese = "N"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 20
+
+    size = "M"
+    pepperoni = "Y"
+    extra_cheese = "Y"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 24
+
+    size = "L"
+    pepperoni = "N"
+    extra_cheese = "N"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 25
+
+    size = "L"
+    pepperoni = "Y"
+    extra_cheese = "N"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 28
+
+    size = "L"
+    pepperoni = "Y"
+    extra_cheese = "Y"
+    assert obj.calculate_bill(size, pepperoni, extra_cheese) == 29
