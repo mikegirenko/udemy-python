3449ead9cb271c3fa23cfe2501e71c5f13325772
mike.girenko <mike.girenko@cybergrx.com>
2023-08-30 15:07:42 -0600

    Completed FizzBuzz homework

---
+++ fizz_buzz/fizz_buzz.py
@@ -0,0 +1,25 @@
+"""
+Write a program which play FizzBuzz game.
+Print each number from 1 to 100.
+If number divisible by 3, print Fizz
+If number divisible by 5, print Buzz
+If number divisible by 3 and 5 (e.g. 15), print FizzBuzz
+"""
+
+
+class FizzBuzz:
+    def play_fizz_buzz(self):
+        for i in range(1, 101):
+            if i % 3 != 0 and i % 5 != 0:
+                print(i)
+            if i % 3 == 0 and i % 5 != 0:
+                print("Fizz")
+            if i % 5 == 0 and i % 3 != 0:
+                print("Buzz")
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+
+
+if __name__ == "__main__":
+    obj = FizzBuzz()
+    obj.play_fizz_buzz()
---
+++ fizz_buzz/test_fizz_buzz.py
@@ -0,0 +1,8 @@
+from fizz_buzz.fizz_buzz import *
+
+obj = FizzBuzz()
+
+
+def test_play_fizz_buzz():
+    # cannot really assert anything because function does not return anything
+    obj.play_fizz_buzz()
