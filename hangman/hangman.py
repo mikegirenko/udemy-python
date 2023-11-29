94eb17c7bb0326fb65b7313e2029192e39d39a3b
mike.girenko <mike.girenko@cybergrx.com>
2023-11-29 15:31:56 -0700

    Working through Hangman - improving logic

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,22 +9,28 @@ class Hangman:
         self.word_list = ["aardvark", "baboon", "camel"]
         self.chosen_word = random.choice(self.word_list)
         print("Chosen word is", self.chosen_word)
-        self.guess = input("Guess a letter: ").lower()
 
     def play_game(self):
         display = []
+        game_over = False
+        good_guess = 0
         for i in range(len(self.chosen_word)):
             display.append("_")
-        print("Initial display", display)
+        print("Initial display:", display)
 
-        for position in range(len(self.chosen_word)):
-            letter = self.chosen_word[position]
-            if letter == self.guess:
-                display[position] = letter
-            else:
-                print("Wrong")
-
-        print("Updated display", display)
+        while not game_over:
+            guess = input("Guess a letter: ").lower()
+            for position in range(len(self.chosen_word)):
+                letter = self.chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+                    good_guess += 1
+                    print("Updated display:", display)
+                    if good_guess == len(self.chosen_word):
+                        game_over = True
+                        print("You won!")
+                if letter != guess: # TODO do not print if guess was correct
+                    print("You wrong")
 
 
 if __name__ == "__main__":
