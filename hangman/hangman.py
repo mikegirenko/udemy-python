ca8e0b0a7da5fad1521dbffb505eeb311c8519ec
mike.girenko <mike.girenko@cybergrx.com>
2024-01-24 14:55:30 -0700

    Hangman was not actually finished. Now program imports from art and words files

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,79 +1,15 @@
 """
-randomly choose a word, ask user to guess a letter, then check if letter exists in that word
+randomly choose a word, ask user to guess a letter, then check if letter exists in that word.
 print ASCII art based on number of lives remaining
 """
 import random
-
-
-stages = [
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========""",
-    """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
-]
+from hangman_words import word_list
+from hangman_art import logo, stages
 
 
 class Hangman:
     def __init__(self):
-        self.word_list = ["aardvark", "baboon", "camel"]
+        self.word_list = word_list
         self.chosen_word = random.choice(self.word_list)
         self.lives = 6
         print("Chosen word is", self.chosen_word)
@@ -84,9 +20,12 @@ class Hangman:
         good_guess = 0
         for i in range(len(self.chosen_word)):
             display.append("_")
+        print(logo)
         print("Initial display:", display)
         while not game_over:
             guess = input("Guess a letter: ").lower()
+            if guess in display:  # this letter was already guessed
+                print(f"You already guessed this letter '{guess}'")
             if guess in self.chosen_word:  # guess is correct
                 list_of_indexes = [
                     pos for pos, char in enumerate(self.chosen_word) if char == guess
@@ -99,6 +38,7 @@ class Hangman:
                     print("You won!")
             if guess not in self.chosen_word:  # guess is not correct
                 self.lives -= 1
+                print(f"Letter '{guess}' is not in the word")
             if self.lives == 0:
                 game_over = True
                 print("You lose!")
